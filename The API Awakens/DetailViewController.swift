16a85c054cf70254ca0046f2ef6ed1e9b59ff432
Gary Luce <lucegary@gmail.com>
2016-09-17 15:57:42 +0100

    Initial commit

---
+++ The API Awakens/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  The API Awakens
+//
+//  Created by Gary Luce on 17/09/2016.
+//  Copyright © 2016 gloos. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
